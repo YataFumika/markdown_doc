7d8bcb3c423684f79808ef50497b4ec42a02f4a1
YataFumika <vulpes1793@gmail.com>
2017-04-15 15:29:03 +0900

    フォーマット変換前のファイルは削除

--- convert_markdown_into_other.sh
+++ convert_markdown_into_other.sh
@@ -4,10 +4,11 @@ CONVERT_TO="html"
 # ./markdowns 内の.mdファイルをtextile形式に変換
 
 for file in `\find ./markdowns/ -name '*.md'`; do
-  new_name=${file%.*}.${CONVERT_TO}		
+  new_name=${file%.*}.${CONVERT_TO}
   # 別フォーマットへ変換
   pandoc -s ${file} -o ${new_name}
   # 変換後のディレクトリへ移動
   mkdir -p ./${CONVERT_TO}s
   mv ${new_name} ./${CONVERT_TO}s/
+  rm ${file}
 done
